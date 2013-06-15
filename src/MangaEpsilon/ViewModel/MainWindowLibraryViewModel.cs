3aaa1f2b91c63cc2fba5d29cf6aac19e9a1ba5cf
Amrykid <amrykid@gmail.com>
2013-06-15 15:52:52 -0500

    Added Library tab and view model.

---
+++ src/MangaEpsilon/ViewModel/MainWindowLibraryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crystal.Core;
+
+namespace MangaEpsilon.ViewModel
+{
+    public class MainWindowLibraryViewModel: BaseViewModel
+    {
+    }
+}
