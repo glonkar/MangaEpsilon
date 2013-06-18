d31b7dab95fd57e1c874147d5a42cbddebfe0df3
Amrykid <amrykid@gmail.com>
2013-06-18 07:59:30 -0500

    Downloaded chapters take on the current theme's accent color in the DetailView.

---
+++ src/MangaEpsilon/Selectors/MangaDetailViewChapterTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Crystal.Messaging;
+using MangaEpsilon.Manga.Base;
+using MangaEpsilon.Services;
+
+namespace MangaEpsilon.Selectors
+{
+    public class MangaDetailViewChapterTemplateSelector: DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate DownloadedTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item,
+          DependencyObject container)
+        {
+            ChapterEntry path = (ChapterEntry)item;
+
+            if (LibraryService.Contains(path))
+                return DownloadedTemplate;
+
+            return DefaultTemplate;
+        }
+    }
+}
